72f82a7f35978cdbb5eecd9943eb6cd382e7ba63
Jun <hjkim.it@gmail.com>
2024-05-26 04:05:36 +0900

    문제1개
    가운데 글자 가져오기

---
+++ lv1 기출/가운데 글자 가져오기.js
@@ -0,0 +1,19 @@
+function solution(s) {
+  let answer = '';
+  const startStr = s.length / 2
+  const str = s.split('')
+
+  if (s.length % 2 === 0) {
+    answer = str[startStr-1].concat(str[startStr])
+    
+  } else {
+    answer = str[startStr - 0.5]
+  }
+  return answer;
+}
+
+//2
+//4
+const s = 'abcdeacde'
+
+solution(s)

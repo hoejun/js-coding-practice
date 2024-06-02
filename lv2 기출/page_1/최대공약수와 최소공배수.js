dec8b9c06f280bd0a00c7e25fe0ff4e03774e0ba
Jun <hjkim.it@gmail.com>
2024-06-02 14:58:24 +0900

    문제 1개
    -최대공약수와 최소공배수

--- lv2 기출/page_1/최대공약수와 최소공배수.js
+++ lv2 기출/page_1/최대공약수와 최소공배수.js
@@ -5,27 +5,31 @@ function solution(n, m) {
   let max = 0
   let min = 0
 
-  if (rightNum % leftNum !== 0 && leftNum % 2 !== 0 && rightNum % 2 !== 0) {
-    max = 1
-  } else {
-    for (let index = 1; index <= leftNum; index++) {
-      if (leftNum % index === 0 && rightNum % index === 0) arr.push(index)
-    }
-    max = Math.max(...arr)
+  // if (leftNum % 2 !== 0 && rightNum % 2 !== 0) {
+  // // if (rightNum % leftNum !== 0 && leftNum % 2 !== 0 && rightNum % 2 !== 0) {
+  //   max = 1
+  // } else {
+  //   for (let index = 1; index <= leftNum; index++) {
+  //     if (leftNum % index === 0 && rightNum % index === 0) arr.push(index)
+  //   }
+  //   max = Math.max(...arr)
+  // }
+  for (let index = 1; index <= leftNum; index++) {
+    if (leftNum % index === 0 && rightNum % index === 0) arr.push(index)
   }
+  max = Math.max(...arr)
+  // if (rightNum % leftNum === 0) {
+  //   min = rightNum
+  // }
+  // else {
+  const s1 = leftNum / max
+  const s2 = rightNum / max
 
-  if (rightNum % leftNum === 0) {
-    min = rightNum
-  }
-  else {
-    const s1 = leftNum / max
-    const s2 = rightNum / max
-    min = max * s1 * s2
-  } 
-
+  min = max * s1 * s2
+  // } 
   return [max, min];
 }
 
-const n = 24
-const m = 18
+const n = 3
+const m = 17
 console.log(solution(n, m))

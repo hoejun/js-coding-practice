4671d418bd85fb8081b93e2d6c413f2e1e5ed165
Jun <hjkim.it@gmail.com>
2024-06-05 01:52:17 +0900

    코드정리
    코드정리

--- lv1 기출/page_2/예산.js
+++ lv1 기출/page_2/예산.js
@@ -6,21 +6,24 @@ function solution(d, budget) {
   for (let index = 0; index < d.length; index++) {
     const min = Math.min(...arr)
     const x = arr.find(i => i === min)
-    for (let i = arr.length - 1; i>=0; i--) {
-      if(arr[i] === x) {
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+      if (arr[i] === x) {
         arr.splice(i, 1)
         break
       }
     }
+
     _budget -= x
 
     if (_budget < 0) return result
+
     result++
   }
   return result;
 }
 
-const d = [1,3,2,5,4]
+const d = [1, 3, 2, 5, 4]
 const budget = 9
 
 console.log(solution(d, budget))

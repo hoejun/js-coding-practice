2c6419f6042b0a2e2545d54ed567bb2b046f6a55
Jun <hjkim.it@gmail.com>
2024-06-09 02:50:06 +0900

    문제 1개
    -삼총사

--- lv1 기출/page_3/삼총사.js
+++ lv1 기출/page_3/삼총사.js
@@ -1,22 +1,15 @@
 function solution(number) {
-  var answer = 0;
   let count = 0
+  // 3중 for문은 되도록이면 사용하지말자.. 로직 이해하기 힘들다..
   for (let index = 0; index < number.length; index++) {
-    // console.log(number[index]);
-    // console.log(number[index+1]);
-    // console.log(number[index+2]);
-    console.log('합계');
-    const num = number[index] + number[index + 1] + number[index + 2]
-
-    if (num === 0) count++
-    // for (let j = 0; j < array.length; index++) {
-    //   const s = number[index] + 
-
-    // }
-
+    for(let j = index+1; j < number.length; j++) {
+      for (let k = j+1; k < number.length; k++) {
+        const num = number[index] + number[j] + number[k]
+        if (num === 0) count++
+      }
+    }
   }
-  console.log(count);
-  return answer;
+  return count;
 }
 
 const number = [-2, 3, 0, 2, -5]

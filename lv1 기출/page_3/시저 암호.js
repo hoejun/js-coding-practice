8eb926971d3ddc5674e07e1dd29b40a21211b006
Jun <hjkim.it@gmail.com>
2024-06-12 00:28:54 +0900

    문제 1개
    - 시저 암호

--- lv1 기출/page_3/시저 암호.js
+++ lv1 기출/page_3/시저 암호.js
@@ -1,9 +1,26 @@
 function solution(s, n) {
-  var answer = '';
-  return answer;
+  let charS = s.split('')
+
+  const newChar = charS.map(i => {
+    if (i !== ' ') {
+      let aci = i.codePointAt() // 아스키 코드 값 얻기
+      aci+=n
+      // 대문자, 소문자 분기 처리해야함 분기처리 없이 계산을 하게 되면 Z에서 소문자로 게산이 됨
+      if (i === i.toUpperCase()) {
+        if (aci > 90) aci = aci-26 // Z 이상일떄
+      }
+      if (i === i.toLowerCase()) {
+        if (aci > 122) aci = aci-26 // z 이상일때
+      }
+      return i = String.fromCharCode(aci)
+    } else {
+      return ' '
+    }
+  })
+  return newChar.join('')
 }
 
-const s = 'AB'
-const n = 1
+const s = 'z'
+const n = 25
 
 solution(s, n)

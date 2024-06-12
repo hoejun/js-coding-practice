ad6764d2d1479e0eea00b1e3061b9930c0589197
Jun <hjkim.it@gmail.com>
2024-06-13 00:12:46 +0900

    문제 1개 - 학습필요
    -짝지어 제거하기

--- lv2 기출/page_1/짝지어 제거하기.js
+++ lv2 기출/page_1/짝지어 제거하기.js
@@ -1,33 +1,16 @@
 function solution(s) {
-  let _s = s.split('')
-  let str = s
-  for (let index = 0; index < _s.length; index++) {
-    // console.log(_s.splice(index, 2));
-    // console.log(object);
-    // console.log('크기', index);
-    // console.log(_s[index]);
-    // console.log(_s[index + 1]);
-    if (_s[index] === _s[index + 1]) {
-      // console.log('들어옴');
-      // console.log(_s.splice(index, 2));
-      _s.splice(index, 2)
-      console.log(_s);
-      index--
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (stack.length && stack[stack.length - 1] === s[i]) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
     }
-    // for (let j = 1; j < _s.length; j++) {
-    //   if (_s[index] !== _s[j]) break
-    //   else 
-    // }
   }
-  console.log(_s);
-  const result = _s ? 0 : 1
-  // console.log(result);
-  var answer = -1;
-
-  return answer;
+  return stack.length ? 0 : 1;
 }
 
-// const s = 'cdcd'
-const s = 'baabaa'
+const s = 'cdcd'
+// const s = 'baabaa'
 
-solution(s)
+console.log(solution(s))
